e9ca97784329b8dbf1ef1746a09bad61bd64b7b6
gaowei1012 <gyb2020018@163.com>
2021-11-27 11:07:11 +0800

    init app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 200,
+  trailingComma: 'none',
+  jsxBracketSameLine: true,
+};
---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>frontend app</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
