f85f1b1b37b72e4b5837d80e71f68a4eda8de509
gaowei1012 <gyb2020018@163.com>
2021-11-28 21:24:44 +0800

    add servers or config

---
+++ src/services/request.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosError, AxiosResponse } from 'axios'
+
+type methodsType = {
+  method: string | 'GET' | 'POST' | 'PUT' | 'DELETE'
+}
+
+export function request(url: string, method: any, data?: object) {
+  return new Promise((resolve, reject) => {
+    let body = method === "GET" ? "params" : "data";
+    axios({
+      url,
+      method,
+      [body]: data === null ? "" : data,
+      headers: {
+        "Content-Type": "application/json",
+        // sessionId: token === null ? "" : token,
+      },
+    })
+      .then((res: AxiosResponse) => {
+        resolve(res)
+      })
+      .catch((err: AxiosError) => {
+        reject(err)
+      })
+  })
+}
