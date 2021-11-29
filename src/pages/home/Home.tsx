25a1b7044b58963b7e070e68377d6c12e9d8529e
gaowei1012 <gyb2020018@163.com>
2021-11-29 10:26:24 +0800

    fix ui

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { View, SafeAreaView, Text } from 'react-native'
-import { Input, Button } from 'react-native-elements'
+// import { Input, Button } from 'react-native-elements'
 
 const Home = () => {
   return (
     <SafeAreaView>
       <View>
-        <Input label='验证码' placeholder='请输入验证码' autoCompleteType inputStyle={{ borderBottomColor: '#fff' }} />
-        <Button buttonStyle={{width: 120, alignSelf: 'center'}} type='clear' title="Solid Button" />
+        <Text>首页</Text>
+        {/* <Input label='验证码' placeholder='请输入验证码' autoCompleteType inputStyle={{ borderBottomColor: '#fff' }} />
+        <Button buttonStyle={{width: 120, alignSelf: 'center'}} type='clear' title="Solid Button" /> */}
       </View>
     </SafeAreaView>
   )
