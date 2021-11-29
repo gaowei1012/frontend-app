ef32867d395d1cf7db6d21f0f36381f548994bea
gaowei1012 <gyb2020018@163.com>
2021-11-29 13:25:30 +0800

    add toast,modal

--- src/pages/home/Detail.tsx
+++ src/pages/home/Detail.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import { Button } from 'react-native-elements'
+import { RootToast } from '../../utils'
 
 
 const Detail = () => {
   return (
     <SafeAreaView>
       <Text>detail</Text>
+      <Button title='show toast' onPress={() => {
+        RootToast.showToast('show toast')
+      }} />
     </SafeAreaView>
   )
 }
